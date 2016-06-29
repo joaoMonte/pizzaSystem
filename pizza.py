4302b525078340ea061ed6291b542ede78fee9a5
joaovlm <jvlm@cin.ufpe.br>
2016-06-29 14:20:18 -0300

    Added functions to generate the table html file.

--- pizza.py
+++ pizza.py
@@ -1,32 +1,4 @@
 #!flask/bin/python
-'''
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/pessoa', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-'''
 
 
 from flask import Flask
@@ -35,15 +7,50 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+dictPerson = {}
+
+def createTable():
+    strHtmlTable = "<head><link rel= \"stylesheet\" type= \"text/css\" href= \"{{ url_for(\'static\',filename=\'styles/table.css\') }}\">"
+    strHtmlTable += "</head><table><thead><tr><th>Nome</th><th>Quantidade</th>"
+    strHtmlTable += "<th>Refrigerante</th><th>Sugest&atilde;o</th></tr></thead><tbody>"
+    tableLines = ""
+
+    for person in dictPerson:
+        qtd = dictPerson[person]['qtd']
+        refri = dictPerson[person]['refri']
+        sabor = dictPerson[person]['sabor']
+        tableLines += createTableLine(person, qtd, refri, sabor)
+
+    strHtmlTable += tableLines
+    strHtmlTable += "</tbody></table>"
+    table = open('table.html', w)
+    table.write(strHtmlTable)
+
+
+def createTableLine(name, qtd, refri, sabor):
+    strHtml = "<tr><td><strong>"
+    strHtml += str(name)
+    strHtml += "</strong></td><td>"
+    strHtml += str(15)
+    strHtml += "</td><td>"
+    strHtml += str(refri)
+    strHtml += "</td><td>"
+    strHtml += str(sabor)
+    strHtml += "</td></tr>"
+    return strHtml
+
+def addPerson(name, qtd, refri, sabor):
+    dictPerson[name] = {'qtd':qtd, 'refri':refri, 'sabor':sabor}
+
+@app.route('/pizza', methods=['GET'])
 def my_form():
     return render_template("form.html")
 
-@app.route('/', methods=['POST'])
+@app.route('/pizza', methods=['POST'])
 def my_form_post():
-
-    text = request.form['text']
-    processed_text = text.upper()
+    print(request.form['id1'])
+    processed_text = text
+    print(text)
     return processed_text
 
 if __name__ == '__main__':
