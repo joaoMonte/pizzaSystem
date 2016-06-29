84a33e0cab776eacdbcfd445a78a223f65ad20db
joaovlm <jvlm@cin.ufpe.br>
2016-06-29 12:34:47 -0300

    Added first files.

---
+++ pizza.py
@@ -0,0 +1,50 @@
+#!flask/bin/python
+'''
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/pessoa', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''
+
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def my_form():
+    return render_template("pizza.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0')
